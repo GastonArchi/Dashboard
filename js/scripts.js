f601e2f775c5fe2e84453be13c8b5a24b572e37d
HALEMAN\u42137296 <u42137296@hospitalaleman.com>
2019-10-29 16:26:27 -0300

    First Commit
    Primer Commit

---
+++ js/scripts.js
@@ -0,0 +1,421 @@
+window.Apex = {
+  chart: {
+    foreColor: '#ccc',
+    toolbar: {
+      show: false
+    },
+  },
+  stroke: {
+    width: 3
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    theme: 'dark'
+  },
+  grid: {
+    borderColor: "#535A6C",
+    xaxis: {
+      lines: {
+        show: true
+      }
+    }
+  }
+};
+
+var spark1 = {
+  chart: {
+    id: 'spark1',
+    group: 'sparks',
+    type: 'line',
+    height: 80,
+    sparkline: {
+      enabled: true
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    }
+  },
+  series: [{
+    data: [25, 66, 41, 59, 25, 44, 12, 36, 9, 21]
+  }],
+  stroke: {
+    curve: 'smooth'
+  },
+  markers: {
+    size: 0
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110
+    }
+  },
+  colors: ['#fff'],
+  tooltip: {
+    x: {
+      show: false
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return '';
+        }
+      }
+    }
+  }
+}
+
+var spark2 = {
+  chart: {
+    id: 'spark2',
+    group: 'sparks',
+    type: 'line',
+    height: 80,
+    sparkline: {
+      enabled: true
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    }
+  },
+  series: [{
+    data: [12, 14, 2, 47, 32, 44, 14, 55, 41, 69]
+  }],
+  stroke: {
+    curve: 'smooth'
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110
+    }
+  },
+  markers: {
+    size: 0
+  },
+  colors: ['#fff'],
+  tooltip: {
+    x: {
+      show: false
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return '';
+        }
+      }
+    }
+  }
+}
+
+var spark3 = {
+  chart: {
+    id: 'spark3',
+    group: 'sparks',
+    type: 'line',
+    height: 80,
+    sparkline: {
+      enabled: true
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    }
+  },
+  series: [{
+    data: [47, 45, 74, 32, 56, 31, 44, 33, 45, 19]
+  }],
+  stroke: {
+    curve: 'smooth'
+  },
+  markers: {
+    size: 0
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110
+    }
+  },
+  colors: ['#fff'],
+  xaxis: {
+    crosshairs: {
+      width: 1
+    },
+  },
+  tooltip: {
+    x: {
+      show: false
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return '';
+        }
+      }
+    }
+  }
+}
+
+var spark4 = {
+  chart: {
+    id: 'spark4',
+    group: 'sparks',
+    type: 'line',
+    height: 80,
+    sparkline: {
+      enabled: true
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    }
+  },
+  series: [{
+    data: [15, 75, 47, 65, 14, 32, 19, 54, 44, 61]
+  }],
+  stroke: {
+    curve: 'smooth'
+  },
+  markers: {
+    size: 0
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110
+    }
+  },
+  colors: ['#fff'],
+  xaxis: {
+    crosshairs: {
+      width: 1
+    },
+  },
+  tooltip: {
+    x: {
+      show: false
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return '';
+        }
+      }
+    }
+  }
+}
+
+new ApexCharts(document.querySelector("#spark1"), spark1).render();
+new ApexCharts(document.querySelector("#spark2"), spark2).render();
+new ApexCharts(document.querySelector("#spark3"), spark3).render();
+new ApexCharts(document.querySelector("#spark4"), spark4).render();
+
+
+var optionsLine = {
+  chart: {
+    height: 250,
+    width: 400,
+    type: 'line',
+    zoom: {
+      enabled: false
+    },
+    dropShadow: {
+      enabled: true,
+      top: 3,
+      left: 2,
+      blur: 4,
+      opacity: 1,
+    }
+  },
+  stroke: {
+    curve: 'smooth',
+    width: 1
+  },
+  //colors: ["#3F51B5", '#2196F3'],
+  series: [{
+      name: "Asesinatos",
+      data: [3, 6, 17, 13, 2, 13]
+    },
+    {
+      name: "Muertes",
+      data: [3, 6, 2, 10, 8, 3]
+    },
+    {
+      name: "Asistencias",
+      data: [0, 15, 20, 11, 9, 3]
+    }
+  ],
+  title: {
+    text: 'K/D/A',
+    align: 'left',
+    offsetY: 15,
+    offsetX: 20
+  },
+  markers: {
+    size: 6,
+    strokeWidth: 0,
+    hover: {
+      size: 9
+    }
+  },
+  grid: {
+    show: true
+  },
+  labels: ['01/15', '01/16', '01/17', '01/18', '01/19', '01/20'],
+  xaxis: {
+    tooltip: {
+      enabled: false
+    }
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'right',
+    offsetY: -20
+  }
+}
+
+var chartLine = new ApexCharts(document.querySelector('#line-adwords'), optionsLine);
+chartLine.render();
+
+var optionsCircle4 = {
+  chart: {
+    type: 'radialBar',
+    width: 380,
+    height: 360,
+  },
+  plotOptions: {
+    radialBar: {
+      size: undefined,
+      inverseOrder: true,
+      hollow: {
+        margin: 5,
+        size: '48%',
+        background: 'transparent',
+
+      },
+      track: {
+        show: false,
+      },
+      startAngle: -180,
+      endAngle: 180
+
+    },
+  },
+  stroke: {
+    lineCap: 'round'
+  },
+  series: [71, 63, 77],
+  labels: ['June', 'May', 'April'],
+  legend: {
+    show: true,
+    floating: true,
+    position: 'right',
+    offsetX: 70,
+    offsetY: 240
+  },
+}
+
+var chartCircle4 = new ApexCharts(document.querySelector('#radialBarBottom'), optionsCircle4);
+chartCircle4.render();
+
+
+var optionsBar = {
+  chart: {
+    height: 270,
+    width: 450,
+    type: 'bar',
+    stacked: true,
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: '30%',
+      horizontal: false,
+    },
+  },
+  series: [{
+    name: 'Daño Físico',
+    data: [600, 2500, 2100, 1700, 1200, 1300, 1100, 1900]
+  }, {
+    name: 'Daño Mágico',
+    data: [1700, 2300, 2000, 800, 1300, 2700, 3300, 1200]
+  }, {
+    name: 'Daño Verdadero',
+    data: [500, 170, 150, 150, 210, 140, 150, 130]
+  }],
+  xaxis: {
+    categories: ['Kai sa', 'Jax', 'Caitlyn', 'Vladimir', 'Annie', 'Ezreal', 'Soraka', 'Kindred'],
+  },
+  fill: {
+    opacity: 1
+  },
+
+}
+
+var chartBar = new ApexCharts(
+  document.querySelector("#barchart"),
+  optionsBar
+);
+
+chartBar.render();
+
+var optionsArea = {
+  chart: {
+    height: 380,
+    type: 'area',
+    stacked: false,
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  series: [{
+      name: "Music",
+      data: [11, 15, 26, 20, 33, 27]
+    },
+    {
+      name: "Photos",
+      data: [32, 33, 21, 42, 19, 32]
+    },
+    {
+      name: "Files",
+      data: [20, 39, 52, 11, 29, 43]
+    }
+  ],
+  xaxis: {
+    categories: ['2011 Q1', '2011 Q2', '2011 Q3', '2011 Q4', '2012 Q1', '2012 Q2'],
+  },
+  tooltip: {
+    followCursor: true
+  },
+  fill: {
+    opacity: 1,
+  },
+
+}
+
+var chartArea = new ApexCharts(
+  document.querySelector("#areachart"),
+  optionsArea
+);
+
+chartArea.render();
